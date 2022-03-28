5e76abaacb8ade551f78215a9fec3f0bd130796e
jpaulo2710 <jpaulo2710@hotmail.com>
2022-03-28 14:47:33 -0300

    commit

---
+++ HTTP/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace HTTP
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ReqList();
+            ReqUnica();
+            Console.ReadLine();
+
+        }
+
+
+        static void ReqList()
+        {
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+            requisicao.Method = "GET";
+            var resposta = requisicao.GetResponse();
+            using (resposta)
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+
+
+        static void ReqUnica()
+        {
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/49");
+            requisicao.Method = "GET";
+            var resposta = requisicao.GetResponse();
+            using (resposta)
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+
+                tarefa.Exibir();
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+
+
+
+    }
+}
---
+++ HTTP/Tarefa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    class Tarefa
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Objeto Tarefa");
+            Console.WriteLine($"User id: {userId}");
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Finalizou?: {completed}");
+            Console.WriteLine("");
+            Console.WriteLine("=========================");
+        }
+
+    }
+}
